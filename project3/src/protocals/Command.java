d170f5b38c212673192ec195d787beb70bed6d7f
RomanCX <zichangfeng@gmail.com>
2014-11-10 15:59:20 -0500

    Update Command

--- project3/src/protocals/Command.java
+++ project3/src/protocals/Command.java
@@ -3,19 +3,14 @@ package protocals;
 import java.util.ArrayList;
 
 public class Command {
-	public enum Operation {
-		FETCH_DATA,
-		DELETE_DATA,
-		READ_DATA,
-		WRITE_DATA
-	}
 	
 	public Operation operation;
 	private ArrayList<Host> targets;
 	private ArrayList<Integer> blockIds;
 	
-	public Command() {
-		
+	public Command(Operation operation, ArrayList<Integer> blockIds) {
+		this.operation = operation;
+		this.blockIds = blockIds;
 	}
 	
 	public ArrayList<Host> getTargets() {
