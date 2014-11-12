d5a3cd0ae8bd0a1f1420ba7037150126f8395a6d
Siyuan Zhou <siyuanzhou@siyuans-mbp.wv.cc.cmu.edu>
2014-11-12 16:59:17 -0500

    throw RemoteException

--- project3/src/Test.java
+++ project3/src/Test.java
@@ -1,7 +1,8 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 
 public interface Test extends Remote {
-	public void kao(Class clazz);
+	public void kao(Class clazz) throws RemoteException;
 
 }
--- project3/src/TestImp.java
+++ project3/src/TestImp.java
@@ -1,15 +1,20 @@
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import protocals.NamenodeProtocal;
 
 
 public class TestImp implements Test {
 
 	public static void main(String[] args) {
 		try {
-			Test test = new TestImp();
+			TestImp test = new TestImp();
+			Test testStub = (Test)UnicastRemoteObject.exportObject(test, 0);
+
 			Registry registry = LocateRegistry.createRegistry(1099);
-			registry.rebind("test", test);
+			registry.rebind("test", testStub);
 			
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
