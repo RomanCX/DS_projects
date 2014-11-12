cfdec8f70e2408fb961c797951fe99684b12f019
Siyuan Zhou <siyuanzhou@siyuans-mbp.wv.cc.cmu.edu>
2014-11-11 21:28:15 -0500

    added more throw exception

--- project3/src/protocals/ClientProtocal.java
+++ project3/src/protocals/ClientProtocal.java
@@ -2,6 +2,7 @@ package protocals;
 
 import java.net.InetAddress;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
@@ -12,16 +13,16 @@ public interface ClientProtocal extends Remote{
 	/*
 	 * Returns hashmap<block id, datanode info>
 	 */
-	public TreeMap<Integer, DatanodeInfo > read(String fileName, String address);
+	public TreeMap<Integer, DatanodeInfo > read(String fileName, String address) throws RemoteException;
 	/*
 	 * Returns hashmap<block id, list of datanode info>
 	 */
-	public TreeMap<Integer, List<DatanodeInfo> >write(String fileName, int splitNum);
+	public TreeMap<Integer, List<DatanodeInfo> >write(String fileName, int splitNum) throws RemoteException;
 	
 	/*
 	 *  Get the size of block
 	 */
-	public int getBlockSize();
+	public int getBlockSize() throws RemoteException;
 	
-	public void delete(String fileName);
+	public void delete(String fileName) throws RemoteException;
 }
