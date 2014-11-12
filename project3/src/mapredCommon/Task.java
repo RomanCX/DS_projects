82d13b00ca1dab30bd6210027de3becdb1011297
RomanCX <zichangfeng@gmail.com>
2014-11-12 18:33:42 -0500

    add jobtracker, tasktracker, command

---
+++ project3/src/mapredCommon/Task.java
@@ -0,0 +1,5 @@
+package mapredCommon;
+
+public class Task {
+
+}
--- project3/src/protocals/JobTrackerProtocol.java
+++ project3/src/protocals/JobTrackerProtocol.java
@@ -1,5 +1,6 @@
 package protocals;
 
 public class JobTrackerProtocol {
-
+	public HeartBeatResponse heartBeat(List<Integer> finishedTasks, int numSlots);
+	public int submitJob(Job job);
 }
