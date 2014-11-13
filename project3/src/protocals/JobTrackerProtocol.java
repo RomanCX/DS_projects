57ef498032d68e6c6c3dc2a35e6ad9a576768b4c
RomanCX <zichangfeng@gmail.com>
2014-11-12 21:10:35 -0500

    add job

--- project3/src/protocals/JobTrackerProtocol.java
+++ project3/src/protocals/JobTrackerProtocol.java
@@ -1,6 +1,15 @@
 package protocals;
 
-public class JobTrackerProtocol {
-	public HeartBeatResponse heartBeat(List<Integer> finishedTasks, int numSlots);
-	public int submitJob(Job job);
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import mapredCommon.Job;
+
+public interface JobTrackerProtocol extends Remote {
+	public TkRegistration register(String address ,int port) throws RemoteException;
+	public HeartBeatResponse heartBeat(List<Integer> finishedTasks, 
+							int numSlots, int taskTrackerId) throws RemoteException;
+	public int submitJob(Job job) throws RemoteException;
+	public JobProgress checkProgress(int jobId) throws RemoteException;
 }
